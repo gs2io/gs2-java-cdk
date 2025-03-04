96700ee06e09ea232903e3aebb399413a81351c9
gs2-ci <ci@gs2.io>
2025-03-04 06:18:04 +0000

    916200115de10981f8efd16701df78491e9bab66

--- src/main/java/io/gs2/cdk/core/model/TransactionSetting.java
+++ src/main/java/io/gs2/cdk/core/model/TransactionSetting.java
@@ -6,24 +6,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TransactionSetting {
-
+    private Boolean enableAtomicCommit = false;
+    private Boolean transactionUseDistributor = false;
+    private Boolean acquireActionUseJobQueue = false;
     private String distributorNamespaceId;
     private String queueNamespaceId;
+
 
     public TransactionSetting(
             TransactionSettingOptions options
     ) {
+        this.enableAtomicCommit = options.enableAtomicCommit;
+        this.transactionUseDistributor = options.transactionUseDistributor;
+        this.acquireActionUseJobQueue = options.acquireActionUseJobQueue;
         this.distributorNamespaceId = options.distributorNamespaceId;
         this.queueNamespaceId = options.queueNamespaceId;
     }
 
     public Map<String, Object> properties() {
-        return new HashMap<>() {
-            {
-                put("EnableAutoRun", true);
-                put("DistributorNamespaceId", distributorNamespaceId);
-                put("QueueNamespaceId", queueNamespaceId);
-            }
-        };
+        var properties = new HashMap<String, Object>();
+
+        if (this.enableAtomicCommit != null) {
+            properties.put("EnableAtomicCommit", this.enableAtomicCommit);
+        }
+        if (this.transactionUseDistributor != null) {
+            properties.put("TransactionUseDistributor", this.transactionUseDistributor);
+        }
+        if (this.acquireActionUseJobQueue != null) {
+            properties.put("AcquireActionUseJobQueue", this.acquireActionUseJobQueue);
+        }
+        if (this.distributorNamespaceId != null) {
+            properties.put("DistributorNamespaceId", this.distributorNamespaceId);
+        }
+        if (this.queueNamespaceId != null) {
+            properties.put("QueueNamespaceId", this.queueNamespaceId);
+        }
+
+        return properties;
     }
 }
